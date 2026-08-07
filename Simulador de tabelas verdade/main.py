daa23b0533e2cbcd9aefb937980092d6c984596e
Lucas Gabriel <lucasferreiraperes29@gmail.com>
2026-08-07 15:44:12 -0300

    atualização do molde da tabela

--- Simulador de tabelas verdade/main.py
+++ Simulador de tabelas verdade/main.py
@@ -1,22 +1,53 @@
-#Input de operador e quantidade de proposições
+#input  de quantas proposições deverão ser simuladas e criação da lista de letras
+quan = int(input("Quantas proposições você deseja simular?"))
 letras = []
-quant = int(input("Insira quantas proposições você quer simular? digite 0 para sair.\n:"))
 i = 0
-while i < quant:
-    letraProp =input("Insira a letra da proposição (Em maiúsculo)")
-    letras.append(letraProp)
 
+#laço para input das letras e armazenamento das proposições
+while i < quan:
+    letra = input("Insira as letras de suas proposições:")
+    letras.append([letra])
     i += 1
 
-        
+print(letras)
 
-op = int(input("Digite 1 para conjunção, 2 para disjunção, 3 para condicional e 4 para bicondicional \n :"))
-tam = (len(letras) - 1)
+'''
 
-
-for i in range(tam):
-    for posi in range(tam):
-        if op == 1:
-            print(letras[posi], "|", (letras[posi] + i))
+Primeira tentativa de montar a tabela, o erro de formação se dava pelo fato de quan ser um numero int que era adicionado
+dentro de camadas = [], fazendo com que ela tivesse vários quan dentro ao invés de camadas definidas pelo valor de quan.
 
 
+#matrizes das tabelas
+camadas = []
+linha = []
+
+#fórmula de possibilidades de valores, 2^n
+linhas = 2 ** quan
+
+#criação das linhas
+for i in range(linhas):
+    camadas.append([])
+    verdade = len(camadas)
+    for j in range(quan):
+        linha.append(0)
+        camadas.append(linhas)
+
+print(camadas)
+print (verdade)
+'''
+
+#Definição da matriz de camadas e contador de linhas
+camadas = []
+
+possibilidades = 2 ** quan
+
+#modelo da tabela verdade
+for linhas in range(possibilidades):
+    linha = []
+
+    for colunas in range(quan):
+        linha.append(0)
+
+    camadas.append(linha)
+
+print(camadas)
